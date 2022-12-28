e4c719d5b9476745060b1b901c5ce3ef93384ac7
duyhung2k4 <111480287+duyhung2k4@users.noreply.github.com>
2022-12-28 16:27:37 +0700

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <stdio.h>
-
+// Bài 1
 int main()
 {
     int arr[100];
