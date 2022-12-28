2269703d601803e89f6a2c9b0153258279687cb8
duyhung2k4 <111480287+duyhung2k4@users.noreply.github.com>
2022-12-28 16:37:02 +0700

    Update bai1.c

--- bai1.c
+++ bai1.c
@@ -1,13 +1,5 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
-// Bài 1
+
 int main()
 {
     int arr[100];
