b1235b6e15d089b80151a4c9e88beb98b6e17d95
duyhung2k4 <111480287+duyhung2k4@users.noreply.github.com>
2022-12-28 16:35:41 +0700

    Add files via upload

---
+++ bai2.c
@@ -0,0 +1,18 @@
+int main()
+{
+    
+    int n;
+    printf("Nhap so n: ");
+    scanf("%d", &n);
+    
+    float s = 0.0;
+    
+    for(int i = 1; i < n; i++) {
+        
+        s += (float) 1 / i; 
+    }
+    
+    printf("Tong S = %f", s);
+
+    return 0;
+}
